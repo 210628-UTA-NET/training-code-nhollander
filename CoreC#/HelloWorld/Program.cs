0d8e394c8cd419011d9d072316c0c11ceb4af21c
NHollander2 <61756948+NHollander2@users.noreply.github.com>
2021-06-28 15:06:32 -0500

    Add files via upload

---
+++ CoreC#/HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿using System; //import
+using HouseFunction;
+/*
+        We use PascalCase for naming convention
+*/
+
+namespace CoreC_
+{
+    class Program
+    {
+        //We can use underscores for private variables to access them easier in our code
+        private static string _first = "Hello";
+        private string _last = "World";
+        /*Main method is the first method that will be run with the command dotnet run
+            Static means that the class does not have to be instantiated to be used
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whatever I want");
+            Test obj = new Test();
+            obj.SomeMethod();
+
+            Console.WriteLine(_first + _last);
+            Console.WriteLine(obj.SomeMethod());
+
+            House objHouse = new House();
+            Console.WriteLine(objHouse.getColor);
+            objHouse.setColor("blue");
+            Console.WriteLine(objHouse.getColor());
+        }
+    }
+
+
+    class Test
+    {
+        public void SomeMethod(){
+            Console.WriteLine("someMethod was called");
+        }
+    }
+}
---
+++ CoreC#/HouseFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HouseFunction
+{
+    public class House
+    {
+        private string _price;
+        private string _color;
+        public House(){
+            Console.WriteLine("This constructor was used");
+            _color = "Red";
+        }
+        //C# way of getting info from fields
+        public string Price{
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = value;
+            }
+        }
+        //Java way of getting info from fields
+        public string getColor(){
+            return _color;
+        }
+        public void setColor(string p_color){
+            _color = p_color;
+        }
+        }
+    }
+
